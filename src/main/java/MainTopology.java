f414e36b71d5994c1fa041168e6a5327c746194a
Junfeng Chen <k-2feng@hotmail.com>
2015-04-01 15:03:57 +0800

    添加集群模式

--- src/main/java/MainTopology.java
+++ src/main/java/MainTopology.java
@@ -1,5 +1,8 @@
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.generated.AlreadyAliveException;
+import backtype.storm.generated.InvalidTopologyException;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.tuple.Fields;
 import bolt.AnalysisParkingTimeBolt;
@@ -11,20 +14,25 @@ import spout.RawMessageSpout;
  * Created by Junfeng on 2015/3/23.
  */
 public class MainTopology {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, AlreadyAliveException, InvalidTopologyException {
         TopologyBuilder builder = new TopologyBuilder();
-        builder.setSpout("spout",new RawMessageSpout());
-        builder.setBolt("bolt1",new NormalizeBolt(),2).shuffleGrouping("spout");
-        builder.setBolt("bolt2",new AnalysisParkingTimeBolt(),2).fieldsGrouping("bolt1",new Fields("parkSpaceCode"));
+        builder.setSpout("RawMessageSpout",new RawMessageSpout());
+        builder.setBolt("NormalizeBolt",new NormalizeBolt(),2).shuffleGrouping("RawMessageSpout");
+        builder.setBolt("AnalysisParkingTimeBolt",new AnalysisParkingTimeBolt(),2).fieldsGrouping("NormalizeBolt",new Fields("parkSpaceCode"));
         //builder.setBolt("timeBolt", new TimeBolt()).shuffleGrouping("bolt1");
 
         Config conf = new Config();
-        conf.setDebug(true);
-        conf.put(Config.TOPOLOGY_DEBUG, true);
 
-        LocalCluster cluster = new LocalCluster();
-        cluster.submitTopology("analysis", conf, builder.createTopology());
+        if(args.length==0) {
+            conf.setDebug(true);
+            conf.put(Config.TOPOLOGY_DEBUG, true);
+            LocalCluster cluster = new LocalCluster();
+            cluster.submitTopology("analysis", conf, builder.createTopology());
+        }else {
+            conf.setNumWorkers(3);
 
+            StormSubmitter.submitTopologyWithProgressBar(args[0], conf, builder.createTopology());
+        }
 
     }
 }
