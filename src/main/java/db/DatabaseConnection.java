9315cf04d9ab2c0b6a9d120b343aff69f2ecc3b7
Junfeng Chen <k-2feng@hotmail.com>
2015-04-09 16:34:28 +0800

    添加数据库连接

---
+++ src/main/java/db/DatabaseConnection.java
@@ -0,0 +1,36 @@
+package db;
+
+
+import org.apache.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by cjf on 2014/9/15.
+ */
+public  class DatabaseConnection {
+    private static Logger logger = Logger.getLogger(DatabaseConnection.class);
+    private static Connection conn;
+    private static String url = "jdbc:mysql://muzhipark.mysql.rds.aliyuncs.com:3306/muzhipark";
+    private static String mysqlUsername="muzhipark";
+    private static String mysqlPassword="52762568Jz";
+
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(url, mysqlUsername, mysqlPassword);
+            logger.info("成功加载MySQL驱动！");
+            //System.out.println("成功加载MySQL驱动！");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConn(){
+        return conn;
+    }
+}
